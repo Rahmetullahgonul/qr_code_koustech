6b51f5ddce523cde79bd3915f01bea88ae3db31b
Rahmetullah Gonul <137273597+Rahmetullahgonul@users.noreply.github.com>
2024-03-06 19:10:30 +0300

    Add files via upload
    Pyzbar kütüphanesi kullanarak qr kodun konumu ve içeriğindeki mesajı veren algoritma.

---
+++ pyzbar_qr_det.py
@@ -0,0 +1,51 @@
+import cv2
+from pyzbar.pyzbar import decode
+
+def detect_qr_code(frame):
+    """
+    QR kodlari tespit et
+    """
+    decode_objcets = decode(frame)
+
+    # her bir qr kodu donguyle isleme
+    for obj in decode_objcets:
+        # qr kodun verisini ve konumunu alma
+        data=obj.data.decode('utf-8')
+        rect_points=obj.rect
+
+        # konumu ve veriyi ekrana yazdirma
+        print("QR kodun verisi:",data)
+        print("QR kodun konumu:",rect_points)
+
+        # qr kod icin dikdortgen ciz
+        cv2.rectangle(frame,(rect_points[0],rect_points[1]),(rect_points[2],rect_points[3]),(0,255,0),2)
+
+    return frame
+
+def main():
+    """
+    QR kod tespit edilecek ana kod
+    """
+
+    cap=cv2.VideoCapture()
+
+    while True:
+        ret,frame=cap.read()
+        if not ret:
+            print("Goruntu girmen lazim")
+            break
+
+        # QR kodlari algila
+        frame_with_qr=detect_qr_code(frame)
+
+        # islenmis cerceveyi goster
+        cv2.imshow("QR kod algilandi",frame_with_qr)
+
+        if cv2.waitKey(1) & 0xFF==ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__=="__main__":
+    main()
